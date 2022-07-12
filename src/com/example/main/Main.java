73c792d7b11f84eb1e6290f6225fcb6cc0a525d2
= <adam.roesner@generation.org>
2022-07-12 08:51:56 -0400

    Project file structure and comments

---
+++ src/com/example/main/Main.java
@@ -0,0 +1,74 @@
+package com.example.main;
+
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to our Java Unit 3 Closing CFU");
+
+      /*
+        Some activities for Array
+      */
+
+        //create an array (not an ArrayList) called favoriteColors that holds five strings with your favoriteColors and print the entire array to the screen.
+
+        //Change the third element of facoriteColors to a different color and print that element to the sceen
+
+        //Declare and initialize and array of type int called numbers to hold 1000 elements (this should be used later in the loop exercise)
+
+        //ArrayList
+        //Declare and initialize an ArrayList so that it holds values of type <Double>.
+
+        //Using the .add() method, add 5 decimal values to the ArrayList and print it to the screen
+
+        //Using the .remove() method, remove the 3rd value in the ArrayList.
+
+        //Print ArrayList to the screen.
+
+        //Now change the last element in the ArrayList and print the new element to the screen
+
+
+    /*
+      Some activities for Loops
+    */
+        // write a for loop that prints out numbers 1-1000 and saves these numbers to the empty array you created earlier
+
+        // write a while loop that prints the elements of the array you used in the previous exercise. Take care to avoid infinite looping!
+
+        //write a do-while loop that does the same.
+
+        // write an enhanced for loop that iterates over this array and prints the result of that number modulus(%) 3
+
+
+    /*
+      Some activities for String methods
+    */
+        // Create two String variables, one holding "Hello" and one holding "world"
+
+
+        // using String methods, concatenate these strings together with a space between them so that it reads "Hello world" and save the resulting String to a new variable
+
+
+        // then, create a for loop that iterates over your new String and prints each letter out using sout
+
+
+
+    /*
+      Combination Exercises
+    */
+
+
+        // Create a variable that stores a String with the value "Testing out String methods"
+
+
+        // Use the String's .split() method with the input of '\s' save an array containing each word of your String variable
+
+
+        // Using a loop of your choosing, loop through the array of strings and print each word to the screen in all caps, along with its length
+
+
+        // CHALLENGE: search the Oracle Documentation to find out how to sort an array, then print the sorted array to the string
+
+    }
+}
